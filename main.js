65621ae1fc8fb00b62b87289db041db51fd6aea1
nurullayevweb <nurullayevweb@gmail.com>
2024-07-16 11:18:12 +0500

    Juft toq game function final

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var elJuftToq = document.querySelector(".js-juft-toq");
 var elError = document.querySelector(".js-error");
-var nmbr=Number(prompt("Juft-Toq o'yini uchun son kiriting"))
 
-if (nmbr<1){
-   elError.textContent = "1 va 1 dan katta son kiriting!";
-}else if (nmbr % 2 ==0){
-   elJuftToq.textContent= "Bu juft son oka"
-}else{
-   elJuftToq.textContent= "Bu toq son oka"
+function checkJuftToq(num) {
+   if (num < 1) {
+      return elError.textContent = "1 va 1 dan katta son kiriting!";
+   } else if (num % 2 == 0) {
+      return elJuftToq.textContent = "Bu juft son oka"
+   } else {
+      return elJuftToq.textContent = "Bu toq son oka"
+   }
 }
+
+
+var nmbr = Number(prompt("Juft-Toq o'yini uchun son kiriting"))
+checkJuftToq(nmbr)
